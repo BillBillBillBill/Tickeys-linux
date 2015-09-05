c42d33ab19ea3276b220012b7bf4da139957d267
BillBillBillBill <billo@qq.com>
2015-09-05 13:32:05 +0800

    2015-09-05 version 0.1.7 1.增加了检查更新的功能

--- tickeys/run.py
+++ tickeys/run.py
@@ -5,12 +5,16 @@ import sys
 import os
 import commands
 
+from __init__ import __version__
+import json
+import requests
 
 __author__ = 'Huang xiongbiao(billo@qq.com)'
 
 
 def run_GUI():
     check_root()
+    check_update()
     try:
         stat, terminalId = commands.getstatusoutput('xdotool getactivewindow')
         from GUI import TickeysApp
@@ -26,6 +30,7 @@ def run_GUI():
 
 def run_CLI():
     check_root()
+    check_update()
     from CLI import CLI
     CLI().cmdloop()
 
@@ -39,7 +44,30 @@ def check_root():
     logger.debug("File path:" + os.path.dirname(__file__))
 
 
+def check_update():
+    try:
+        logger.debug("Version checking...")
+        r = requests.get("http://billbill.sinaapp.com/tickeys")
+        returnInfor = json.loads(r.text)
+        # print returnInfor
+        if returnInfor["version"] <= __version__:
+            logger.debug("It is the latest version...")
+            return
+        else:
+            # show update notify
+            import pynotify
+            pynotify.init('Tickeys')
+            title = '<h2>Tickeys</h2>'
+            body = '<span style="color: #00B8CB; font-size:15px">Tickeys</span>有可用的<span style="color: #FF4500">更新：</span>\n 版本：%s \n 内容：%s' % (returnInfor["version"], returnInfor["update"])
+            iconfile = os.getcwd() + '/tickeys.png'
+            notify = pynotify.Notification(title, body, iconfile)
+            notify.show()
+    except Exception, e:
+        logger.error("Version check fail:" + str(e))
+
+
 def run():
+    logger.debug("Tickeys start........")
     if len(sys.argv) != 2 or sys.argv[1] not in ['-c', '-g']:
         print "Usage: python run.py -c     ---CLI mode"
         print "       python run.py -g     ---GUI mode"
